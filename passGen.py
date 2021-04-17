bd39ae74d98c8bf1c17aa1254ca90e26db62e49b
katie241 <katieholmes241@gmail.com>
2021-04-16 23:52:15 -0700

    password generator

--- passGen.py
+++ passGen.py
@@ -1,2 +1,11 @@
+import random
+import string
+
 def passGen(a):
-    pass
+    letters = string.ascii_lowercase
+    numbers = string.digits
+
+    combine = letters + numbers
+    rand = random.sample(combine, a)
+    password = "".join(rand)
+    print(password)
